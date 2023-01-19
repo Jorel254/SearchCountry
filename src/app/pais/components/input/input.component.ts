0840dc664ea7d5bfc2d7c0c192ef897202603627
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2023-01-18 22:41:10 -0600

    Ajustes de los componentes

--- src/app/pais/components/input/input.component.ts
+++ src/app/pais/components/input/input.component.ts
@@ -4,30 +4,25 @@ import { Subject, debounceTime } from 'rxjs';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
-
   SearchText: string = '';
   debouncer: Subject<string> = new Subject();
 
-  @Output() OnEneter: EventEmitter<string>= new EventEmitter();
-  @Output() OnDebounce: EventEmitter<string>= new EventEmitter();
-  constructor() { }
+  @Output() OnEneter: EventEmitter<string> = new EventEmitter();
+  @Output() OnDebounce: EventEmitter<string> = new EventEmitter();
+  constructor() {}
 
   ngOnInit(): void {
-    this.debouncer
-    .pipe(
-      debounceTime(300)
-    )
-    .subscribe(value => {
+    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
       this.OnDebounce.emit(value);
-    })
+    });
   }
-  Search(){
+  Search() {
     this.OnEneter.emit(this.SearchText);
   }
-  KeyPress(){
+  KeyPress() {
     this.debouncer.next(this.SearchText);
   }
 }
